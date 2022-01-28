bd81d7b27b26d3cc1c5f611cd3103c81f54f5683
TakashiYAMAGUCHI <tkshyamaguchi45@gmail.com>
2022-01-28 12:18:22 +0900

    pug

--- webpack.config.js
+++ webpack.config.js
@@ -37,15 +37,34 @@ module.exports = {
           //   }
           // }
         ]
-      }
-    ]
+      },
+      {
+        test: /\.pug/,
+        use: [
+          {
+            loader: 'html-loader',
+          },
+          {
+            loader: 'pug-html-loader',
+            options: {
+              pretty: true,
+            },
+          },
+        ],
+      },
+    ],
   },
   plugins: [
     new MiniCssExtractPlugin({
       filename: './stylesheets/my.css',
     }),
     new HtmlWebpackPlugin({
-      template: './src/templates/index.html',
+      template: './src/templates/index.pug',
+      filename: 'index.html',
+    }),
+    new HtmlWebpackPlugin({
+      template: './src/templates/access.pug',
+      filename: 'access.html',
     }),
     new CleanWebpackPlugin(),
   ]
